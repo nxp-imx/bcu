1d6f95843a9903449e88f00ef175222d2d83eadd
Clark Wang <xiaoning.wang@nxp.com>
2020-04-07 13:25:26 +0800

    fix reseting from sd for imx8dxl_ddr3_evk
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- board.c
+++ board.c
@@ -106,22 +106,22 @@ struct mapping imx8dxl_ddr3[] = {
 	{"vdd_vtt",power,IMX8XXL_POWER_PATH(2,3,10,10), 0x00},
 	{"vdd_ddr_pll",power,IMX8XXL_POWER_PATH(2,4, 1000, 1000), 0x00},
 
-	{"boot_mode",gpio,IMX8XXL_EXP_PATH(0,0x07), 0x40},
-	{"sd_pwr",gpio, IMX8XXL_EXP_PATH(0,0x08), 0x00},
-	{"sd_wp",gpio, IMX8XXL_EXP_PATH(0,0x10), 0x00},
-	{"sd_cd",gpio, IMX8XXL_EXP_PATH(0,0x20), 0x00},
+	{"boot_mode",gpio,IMX8XXL_EXP_PATH(0,0x07), 0x80},
+	{"sd_pwr",gpio, IMX8XXL_EXP_PATH(0,0x08), 0x51},
+	{"sd_wp",gpio, IMX8XXL_EXP_PATH(0,0x10), 0x61},
+	{"sd_cd",gpio, IMX8XXL_EXP_PATH(0,0x20), 0x71},
 	{"exp5_p06", gpio, IMX8XXL_EXP_PATH(0, 0x40), 0x00},
 	{"exp5_p07", gpio, IMX8XXL_EXP_PATH(0, 0x80), 0x00},
 	{"exp5_p14", gpio, IMX8XXL_EXP_PATH(1,0x10), 0x00},
 	{"exp5_p15", gpio, IMX8XXL_EXP_PATH(1,0x20), 0x00},
 	{"testmod_sel",gpio, IMX8XXL_EXP_PATH(,0x40), 0x00},
-	{"bootmode_sel",gpio, IMX8XXL_EXP_PATH(1,0x80), 0x50},
+	{"bootmode_sel",gpio, IMX8XXL_EXP_PATH(1,0x80), 0x90},
 
 	{"reset",gpio, "/ft4232h_gpio{channel=0;pin_bitmask=0x20}", 0x11},
 	{"jtag_sel",gpio, "/ft4232h_gpio{channel=0;pin_bitmask=0x40}", 0x01},
 	{"onoff", gpio, "/ft4232h_gpio{channel=0;pin_bitmask=0x80}", 0x21},
-	{"remote_en",gpio, "/ft4232h_gpio{channel=1;pin_bitmask=0x20}", 0x30},
-	{"ft_reset", gpio, "/ft4232h_gpio{channel=1;pin_bitmask=0x40}", 0x71},
+	{"remote_en",gpio, "/ft4232h_gpio{channel=1;pin_bitmask=0x20}", 0x40},
+	{"ft_reset", gpio, "/ft4232h_gpio{channel=1;pin_bitmask=0x40}", 0x31},
 
 	// you put all the pin in the imx8xxl board here
 	//"sd_wp", gpio, "..."
