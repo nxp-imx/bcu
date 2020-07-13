b5a0ceba5b571041897e0fb934a8c9de7b3b09c6
Clark Wang <xiaoning.wang@nxp.com>
2020-07-13 10:52:16 +0800

    add libcurl submodule for linux to build static lib
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

---
+++ build_libcurl.sh
@@ -0,0 +1,6 @@
+cd libcurl/
+mkdir build.bcu
+cd build.bcu
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DCURL_STATICLIB=ON -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DBUILD_TESTING=OFF -DCMAKE_USE_OPENSSL=ON -DCURL_ZLIB=OFF
+cmake --build . --config Release --clean-first
+cd ../../
