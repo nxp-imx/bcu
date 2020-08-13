e0fffda65df9144adacf3e353c00a28993f289f4
Clark Wang <xiaoning.wang@nxp.com>
2020-08-13 11:13:45 +0800

    fix the information for testmod_sel on 8dxl
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- board.c
+++ board.c
@@ -82,7 +82,7 @@ struct mapping imx8xxl[] = {
 	{"SR_vdd_ana",gpio, IMX8XXL_EXP_PATH(1,0x08), 0x00},
 	{"exp5_p14", gpio, IMX8XXL_EXP_PATH(1,0x10), 0x00},
 	{"exp5_p15", gpio, IMX8XXL_EXP_PATH(1,0x20), 0x00},
-	{"testmod_sel",gpio, IMX8XXL_EXP_PATH(,0x40), 0x00},
+	{"testmod_sel",gpio, IMX8XXL_EXP_PATH(1,0x40), 0x00},
 	{"bootmode_sel",gpio, IMX8XXL_EXP_PATH(1,0x80), 0x90},
 
 	{"reset",gpio, "/ft4232h_gpio{channel=0;pin_bitmask=0x20}", 0x11},
