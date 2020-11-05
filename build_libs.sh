60d3be478b86e11d60df011c9ceacc1bff9dc1ae
Clark Wang <xiaoning.wang@nxp.com>
2020-11-05 20:03:14 +0800

    enable libftdi static lib build
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- build_libs.sh
+++ build_libs.sh
@@ -4,3 +4,8 @@ cd build.bcu
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DCURL_STATICLIB=ON -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DBUILD_TESTING=OFF -DCMAKE_USE_OPENSSL=ON -DCURL_ZLIB=OFF
 cmake --build . --config Release --clean-first
 cd ../../
+
+cd libftdi/
+cmake .
+make
+cd ../
