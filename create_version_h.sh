85640964a9576a213739b34e3765d6d749fbbede
Clark Wang <xiaoning.wang@nxp.com>
2021-03-01 10:15:33 +0800

    fix tag name
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- create_version_h.sh
+++ create_version_h.sh
@@ -4,7 +4,7 @@ if [ "${APPVEYOR_BUILD_VERSION}" = "" ];
 then
 	echo build not in appveyor
 else
-	git tag bcu_${APPVEYOR_BUILD_VERSION}
+	git tag ${APPVEYOR_BUILD_VERSION}
 fi
 
 version=`git describe --tags --long`
