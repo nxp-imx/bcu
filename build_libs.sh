72b7412b65e1b2e9d5b2f68ebee87300a63bf3f1
Clark Wang <xiaoning.wang@nxp.com>
2021-04-02 11:03:01 +0800

    remove the libftdi static link
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- build_libs.sh
+++ build_libs.sh
@@ -4,8 +4,3 @@ cd ${1}/libcurl/build.bcu
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DCURL_STATICLIB=ON -DCMAKE_FIND_LIBRARY_SUFFIXES=.a -DBUILD_TESTING=OFF -DCMAKE_USE_OPENSSL=ON -DCURL_ZLIB=OFF
 cmake --build . --config Release --clean-first
 cd ../../
-
-cd ${1}/libftdi/
-cmake .
-make
-cd ../
