6c01d755bf68506d0f59a2482dd01db9eac38fae
Clark Wang <xiaoning.wang@nxp.com>
2021-04-02 15:35:34 +0800

    update copyright years
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

--- bcu_parser.h
+++ bcu_parser.h
@@ -1,5 +1,5 @@
 /*
-* Copyright 2019 NXP.
+* Copyright 2019-2021 NXP.
 *
 * Redistribution and use in source and binary forms, with or without modification,
 * are permitted provided that the following conditions are met:
