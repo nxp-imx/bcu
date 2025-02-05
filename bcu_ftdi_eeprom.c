c8d8de6bd1fa08d587ccaaefe9d94b940e55961d
Cristian Patru <cristian.patru@nxp.com>
2025-02-05 14:36:33 +0200

    added val_board_11

--- bcu_ftdi_eeprom.c
+++ bcu_ftdi_eeprom.c
@@ -77,6 +77,7 @@ struct ftdi_eeprom_field_code_tb ftdi_eeprom_field[] = {
 	{ftdi_eeprom_board_id, 0x22,"NXP VAL_BOARD_9 Board"},
 	{ftdi_eeprom_board_id, 0x23,"NXP VAL_BOARD_10 Board"},
 	{ftdi_eeprom_board_id, 0x24,"NXP i.MX943 19x19 EVK Board"},
+	{ftdi_eeprom_board_id, 0x25,"NXP VAL_BOARD_11 Board"},
 
 	{ftdi_eeprom_soc_id, 0x1, "i.MX8DXL"},
 	{ftdi_eeprom_soc_id, 0x2, "i.MX8MP"},
@@ -102,6 +103,7 @@ struct ftdi_eeprom_field_code_tb ftdi_eeprom_field[] = {
 	{ftdi_eeprom_soc_id, 0x16, "VAL_BOARD_8 SOC"},
 	{ftdi_eeprom_soc_id, 0x17, "VAL_BOARD_9 SOC"},
 	{ftdi_eeprom_soc_id, 0x18, "VAL_BOARD_10 SOC"},
+	{ftdi_eeprom_soc_id, 0x19, "VAL_BOARD_11 SOC"},
 
 	{ftdi_eeprom_pmic_id, 0x1, "PPF7100BVMA1ES"},  //On imx8dxl-evk
 	{ftdi_eeprom_pmic_id, 0x2, "PCA9450CHN"},  //On imx8mpevk-cpu and imx8mpevk-cpu-pwr
@@ -131,6 +133,7 @@ struct ftdi_eeprom_field_code_tb ftdi_eeprom_field[] = {
 	{ftdi_eeprom_pmic_id, 0x20, "VAL_BOARD_8 PMIC"},
 	{ftdi_eeprom_pmic_id, 0x21, "VAL_BOARD_9 PMIC"},
 	{ftdi_eeprom_pmic_id, 0x22, "VAL_BOARD_10 PMIC"},
+	{ftdi_eeprom_pmic_id, 0x23, "VAL_BOARD_11 PMIC"},
 
 	{0, 0, NULL}//null terminated
 };
@@ -456,6 +459,15 @@ struct ftdi_eeprom_user_area val_board_10_ftdi_eeprom_user_area_info = {
 	0x18, 0x1, 0x1,	//soc_id, soc_rev_number, soc_rev_char
 	0x22, 0x1, 0x1,	//pmic_id, pmic_rev_number, pmic_rev_char
 	0,		//no of power rails
+	1
+};
+
+struct ftdi_eeprom_user_area val_board_11_ftdi_eeprom_user_area_info = {
+	0x1,		//config
+	0x25, 0x3, 0xa,	//board_id, board_rev_number, board_rev_char
+	0x19, 0x1, 0x1,	//soc_id, soc_rev_number, soc_rev_char
+	0x23, 0x1, 0x1,	//pmic_id, pmic_rev_number, pmic_rev_char
+	16,		//no of power rails
 	1
 };
 
