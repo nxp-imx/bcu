nxp-imx/bcu

commit f3d0f943cfa35566248ceebeb45e485cd9289f84

LF-14105 fix the issue that -auto does not work in the Windows platform
Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

chip.c
@@ -358,7 +358,12 @@ void* ft4232h_eeprom_create(char* chip_specification, void* parent)
 		printf("malloc failed\n");
 		return NULL;
 	}
+
+#if defined(__linux__) || defined(__APPLE__)
 	ftee->ftdi_info = &g_ftdi_info[0];
+#else
+	ftee->ftdi_info = &g_ftdi_info[1];
+#endif
 	ftee->eeprom_device.device.parent = parent;
 	ftee->eeprom_device.eeprom_read = ft4232h_eeprom_read;
 	ftee->eeprom_device.eeprom_write = ft4232h_eeprom_write;
@@ -498,7 +503,11 @@ void ft4232h_i2c_remove_all(int enable_1_exit_handler)
 	int i;
 	for (i = 0; i < MAX_FT_I2C_CHANNEL_NUMBER; i++)
 	{
+#ifdef _WIN32
+		if (i == 1)
+#else
 		if (!enable_1_exit_handler && i == 1)
+#endif
 			continue;
 
 		if (g_ftdi_info[i].isinit)

commit 783623ce26d5d4e3211ddf467ccf8d130edf81ad

fix Windows cannot use -board= in monitor cmd issue
Fixes: f3d0f94 ("LF-14105 fix the issue that -auto does not work in the Windows platform")
Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>

chip.c
@@ -359,10 +359,10 @@ void* ft4232h_eeprom_create(char* chip_specification, void* parent)
 		return NULL;
 	}
 
-#if defined(__linux__) || defined(__APPLE__)
 	ftee->ftdi_info = &g_ftdi_info[0];
-#else
-	ftee->ftdi_info = &g_ftdi_info[1];
+#ifdef _WIN32
+	if (ftee->ftdi_info->ftdi == NULL && g_ftdi_info[1].ftdi)
+		ftee->ftdi_info = &g_ftdi_info[1];
 #endif
 	ftee->eeprom_device.device.parent = parent;
 	ftee->eeprom_device.eeprom_read = ft4232h_eeprom_read;