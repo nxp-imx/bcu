1bc442a0f8a60f28d2dd4743eabaf0479bf30720
Costin Constantin <costin.constantin@nxp.com>
2026-04-08 10:54:17 +0300

    Correct wrong serial no. display
    Fix issue of wrongly displaying the serial no. with lsftdi command.
    
    Signed-off-by: Costin Constantin <costin.constantin@nxp.com>

--- port.c
+++ port.c
@@ -366,6 +366,8 @@ void ft_list_devices(char location_str[][MAX_LOCATION_ID_LENGTH], int *board_num
 				if (strcmp("A", &Description[strlen(Description) - 1]) == 0) {
 					board_table[detected_boards][0] = loc_id;
 					sprintf(location_id_str[detected_boards], "%x", loc_id);
+					strncpy(board_serial[detected_boards], SerialNumber, strlen(SerialNumber) - 1);
+					board_serial[detected_boards][strlen(SerialNumber) - 1] = '\0';
 					detected_boards++;
 				}
 				else if (strcmp("B", &Description[strlen(Description) - 1]) == 0) {
